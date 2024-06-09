f3ead1515de11dfab664af4fc52a8fb1e1847a79
Inahoto <inahotoa@nousinfo.com>
2024-06-09 15:18:52 +0530

    Update task1ReadCSVFile.java

--- src/test/java/Rakuten/RakutenTest1/task1ReadCSVFile.java
+++ src/test/java/Rakuten/RakutenTest1/task1ReadCSVFile.java
@@ -14,27 +14,38 @@ public class task1ReadCSVFile {
 
 	public static void main(String[] args) throws IOException {
 
-		// Calling the method 
+		// Calling the method which contains the Logics for getting the Unique Dog Breed
+		// name
 		Set<String> uniqueBreedColumnData = GetUniqueColumnValuesByName("Breed");
 		for (String uniqueBreedName : uniqueBreedColumnData) {
-			System.out.println("Breed: " + uniqueBreedName);
+			// on every iteration 'uniqueBreedName' is removing any white spaces and
+			// converting to lower case
+			String formatedBreedNames = uniqueBreedName.replaceAll("\\s", "").toLowerCase();
+			// Print the 'formatedBreedNames' in the console logs
+			System.out.println("Breed: " + formatedBreedNames);
 		}
 
 	}
-	
-	
-	//Declared the CSV File path as static final so that it remain constant and unchanged
+
+	// Declared the CSV File path as static final so that it remain constant and
+	// unchanged
 	private static final String CSVFilePath = System.getProperty("user.dir") + "\\data\\rakutenCSV.csv";
-	//Function to read the Breed column value 
+
+	// Function to read the Breed column Header and its Column values
 	public static Set<String> GetUniqueColumnValuesByName(String columnHeaderName) {
-		//Using HashSet java collections to store only unique values and eliminate duplicate if found
+		// Using HashSet java collections to store only unique values and eliminate
+		// duplicate if found
 		Set<String> uniqueData = new HashSet<>();
 		try (
-				// opens the csv file for reading
+				// FileReader is used to open and read the content of the CSV file
 				FileReader cscFilereader = new FileReader(CSVFilePath);
 
-				// Parses the CSV file and allows iteration over records
+				// It is a class provided by Apache Commons API. It is used to parsing the CSV
+				// file.
+				// CSVParser reads the content of a CSV file and parses it into individual
+				// records (rows) and fields (columns)
 				CSVParser csvParser = new CSVParser(cscFilereader, CSVFormat.DEFAULT.withFirstRecordAsHeader())) {
+			//using for each loop to add the values to HashSet.
 			for (CSVRecord csvRecord : csvParser) {
 				String columnValue = csvRecord.get(columnHeaderName);
 				uniqueData.add(columnValue);
